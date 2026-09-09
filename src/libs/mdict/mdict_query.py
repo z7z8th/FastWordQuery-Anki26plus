fff9a7fbc3c4a63b4f3228537ee00bd7987abf58
Bob <noreply@github.com>
2026-09-09 08:41:43 +0800

    upgrade readmdict.py to mdict-utils 2025 Jan 2 version. add get_index to readmdict.py MDX/MDD. fix sqlite3.connect file descriptor leak caused failure "OSError too many files" when multi thread querying 10K+ words.

--- src/libs/mdict/mdict_query.py
+++ src/libs/mdict/mdict_query.py
@@ -4,6 +4,8 @@ import json
 import os
 import re
 import sqlite3
+from contextlib import closing
+import threading
 import sys
 # zlib compression is used for engine version >=2.0
 import zlib
@@ -38,6 +40,8 @@ class IndexBuilder(object):
                  enable_history=False,
                  sql_index=True,
                  check=False):
+        self._tlocal_mdx_db_conn = threading.local()
+        self._tlocal_mdd_db_conn = threading.local()
         self._mdx_file = fname
         self._mdd_file = ""
         self._encoding = ''
@@ -47,78 +51,54 @@ class IndexBuilder(object):
         self._description = ''
         self._sql_index = sql_index
         self._check = check
-        _filename, _file_extension = os.path.splitext(fname)
+        _filename_base, _file_extension = os.path.splitext(fname)
         assert (_file_extension == '.mdx')
         assert (os.path.isfile(fname))
-        self._mdx_db = _filename + ".mdx.db"
-        # make index anyway
-        if force_rebuild:
-            self._make_mdx_index(self._mdx_db)
-            if os.path.isfile(_filename + '.mdd'):
-                self._mdd_file = _filename + ".mdd"
-                self._mdd_db = _filename + ".mdd.db"
-                self._make_mdd_index(self._mdd_db)
+        self._mdx_db_path = _filename_base + ".mdx.db"
 
-        if os.path.isfile(self._mdx_db):
-            #read from META table
-            conn = sqlite3.connect(self._mdx_db)
-            #cursor = conn.execute("SELECT * FROM META")
-            cursor = conn.execute("SELECT * FROM META WHERE key = \"version\"")
-            #判断有无版本号
-            for cc in cursor:
-                self._version = cc[1]
-            ################# if not version in fo #############
-            if not self._version:
-                print("version info not found")
-                conn.close()
-                self._make_mdx_index(self._mdx_db)
-                print("mdx.db rebuilt!")
-                if os.path.isfile(_filename + '.mdd'):
-                    self._mdd_file = _filename + ".mdd"
-                    self._mdd_db = _filename + ".mdd.db"
-                    self._make_mdd_index(self._mdd_db)
-                    print("mdd.db rebuilt!")
-                return None
-            cursor = conn.execute(
-                "SELECT * FROM META WHERE key = \"encoding\"")
-            for cc in cursor:
-                self._encoding = cc[1]
-            cursor = conn.execute(
-                "SELECT * FROM META WHERE key = \"stylesheet\"")
-            for cc in cursor:
-                self._stylesheet = json.loads(cc[1])
+        build_mdx_index = False
 
-            cursor = conn.execute("SELECT * FROM META WHERE key = \"title\"")
-            for cc in cursor:
-                self._title = cc[1]
+        try:
+            with closing(sqlite3.connect(self._mdx_db_path)) as conn:
+                #判断有无版本号
+                self._version = self._query_meta(conn, "version")
+                ################# if not version in fo #############
+                if not self._version:
+                    print("*** version info not found, rebuilding!")
+                    build_mdx_index = True
+                else:
+                    self._encoding = self._query_meta(conn, "encoding")
+                    self._stylesheet = json.loads(self._query_meta(conn, "stylesheet"))
+                    self._title = self._query_meta(conn, "title")
+                    self._description = self._query_meta(conn, "description")
+        except Exception as e:
+            build_mdx_index = True
 
-            cursor = conn.execute(
-                "SELECT * FROM META WHERE key = \"description\"")
-            for cc in cursor:
-                self._description = cc[1]
+        if force_rebuild or build_mdx_index:     
+            mdx = MDX(self._mdx_file)
+            self._make_md_index(mdx, self._mdx_db_path, unique=False)
+            self._make_mdx_meta(mdx, self._mdx_db_path)
 
-            #for cc in cursor:
-            #    if cc[0] == 'encoding':
-            #        self._encoding = cc[1]
-            #        continue
-            #    if cc[0] == 'stylesheet':
-            #        self._stylesheet = json.loads(cc[1])
-            #        continue
-            #    if cc[0] == 'title':
-            #        self._title = cc[1]
-            #        continue
-            #    if cc[0] == 'title':
-            #        self._description = cc[1]
-        else:
-            self._make_mdx_index(self._mdx_db)
+        if os.path.isfile(_filename_base + ".mdd"):
+            self._mdd_file = _filename_base + ".mdd"
+            self._mdd_db_path = _filename_base + ".mdd.db"
+            if not os.path.isfile(self._mdd_db_path):
+                self._make_md_index(MDD(self._mdd_file), self._mdd_db_path, unique=True)
 
-        if os.path.isfile(_filename + ".mdd"):
-            self._mdd_file = _filename + ".mdd"
-            self._mdd_db = _filename + ".mdd.db"
-            if not os.path.isfile(self._mdd_db):
-                self._make_mdd_index(self._mdd_db)
-        pass
+    
+    def __del__(self):
+        if hasattr(self._tlocal_mdx_db_conn, "connection"):
+            self._tlocal_mdx_db_conn.connection.close()
+        if hasattr(self._tlocal_mdd_db_conn, "connection"):
+            self._tlocal_mdd_db_conn.connection.close()
 
+    def _query_meta(self, conn, key):
+        cursor = conn.execute(
+                f"SELECT * FROM META WHERE key = \"{key}\"")
+        for cc in cursor:
+            return cc[1]
+        return ''
+    
     def _replace_stylesheet(self, txt):
         # substitute stylesheet definition
         encoding = 'utf-8'
@@ -126,8 +106,8 @@ class IndexBuilder(object):
             # encode_type = chardet.detect(txt)
             # encoding = encode_type['encoding']
             txt = txt.decode(encoding)
-        txt_list = re.split('`\d+`', txt)
-        txt_tag = re.findall('`\d+`', txt)
+        txt_list = re.split(r'`\d+`', txt)
+        txt_tag = re.findall(r'`\d+`', txt)
         txt_styled = txt_list[0]
         for j, p in enumerate(txt_list[1:]):
             style = self._stylesheet[txt_tag[j][1:-1]]
@@ -138,100 +118,82 @@ class IndexBuilder(object):
                 txt_styled = txt_styled + style[0] + p + style[1]
         return txt_styled.encode(encoding)
 
-    def _make_mdx_index(self, db_name):
-        if os.path.exists(db_name):
-            os.remove(db_name)
-        mdx = MDX(self._mdx_file)
-        self._mdx_db = db_name
-        returned_index = mdx.get_index(check_block=self._check)
-        index_list = returned_index['index_dict_list']
-        conn = sqlite3.connect(db_name)
-        c = conn.cursor()
-        c.execute(''' CREATE TABLE MDX_INDEX
-               (key_text text not null,
-                file_pos integer,
-                compressed_size integer,
-                decompressed_size integer,
-                record_block_type integer,
-                record_start integer,
-                record_end integer,
-                offset integer
-                )''')
+    def _make_mdx_meta(self, mdx, db_path):
+        meta = mdx.get_meta()
+        _stylesheet = json.dumps(meta['stylesheet'])
 
-        tuple_list = [(item['key_text'], item['file_pos'],
-                       item['compressed_size'], item['decompressed_size'],
-                       item['record_block_type'], item['record_start'],
-                       item['record_end'], item['offset'])
-                      for item in index_list]
-        c.executemany('INSERT INTO MDX_INDEX VALUES (?,?,?,?,?,?,?,?)',
-                      tuple_list)
-        # build the metadata table
-        meta = returned_index['meta']
-        c.execute('''CREATE TABLE META
-               (key text,
-                value text
-                )''')
+        with closing(sqlite3.connect(db_path)) as conn:
+            with conn:
+                # build the metadata table
+                c = conn.cursor()
+                c.execute('''CREATE TABLE META
+                        (key text,
+                         value text
+                        )''')
 
-        #for k,v in meta:
-        #    c.execute(
-        #    'INSERT INTO META VALUES (?,?)',
-        #    (k, v)
-        #    )
+                #for k,v in meta:
+                #    c.execute(
+                #    'INSERT INTO META VALUES (?,?)',
+                #    (k, v)
+                #    )
 
-        c.executemany('INSERT INTO META VALUES (?,?)',
-                      [('encoding', meta['encoding']),
-                       ('stylesheet', meta['stylesheet']),
-                       ('title', meta['title']),
-                       ('description', meta['description']),
-                       ('version', version)])
+                c.executemany('INSERT INTO META VALUES (?,?)',
+                            [('encoding', meta['encoding']),
+                            ('stylesheet', _stylesheet),
+                            ('title', meta['title']),
+                            ('description', meta['description']),
+                            ('version', version)])
 
-        if self._sql_index:
-            c.execute('''
-                CREATE INDEX key_index ON MDX_INDEX (key_text)
-                ''')
 
-        conn.commit()
-        conn.close()
-        #set class member
         self._encoding = meta['encoding']
-        self._stylesheet = json.loads(meta['stylesheet'])
+        self._stylesheet = json.loads(_stylesheet)
         self._title = meta['title']
         self._description = meta['description']
 
-    def _make_mdd_index(self, db_name):
-        if os.path.exists(db_name):
-            os.remove(db_name)
-        mdd = MDD(self._mdd_file)
-        self._mdd_db = db_name
-        index_list = mdd.get_index(check_block=self._check)
-        conn = sqlite3.connect(db_name)
-        c = conn.cursor()
-        c.execute(''' CREATE TABLE MDX_INDEX
-               (key_text text not null unique,
-                file_pos integer,
-                compressed_size integer,
-                decompressed_size integer,
-                record_block_type integer,
-                record_start integer,
-                record_end integer,
-                offset integer
-                )''')
+    def _make_md_index(self, md, db_path, unique):
+        if os.path.exists(db_path):
+            os.remove(db_path)
+        self._mdd_db_path = db_path
+        index_list = md.get_index(check_block=self._check)
+        print(f'_make_md_index {db_path} index list size {len(index_list)}')
+        with closing(sqlite3.connect(db_path)) as conn:
+            print(f"*** conn {conn}")
+            with conn:
+                c = conn.cursor()
+                c.execute(f''' CREATE TABLE MDX_INDEX
+                    (key_text text not null,
+                        file_pos integer,
+                        compressed_size integer,
+                        decompressed_size integer,
+                        record_block_type integer,
+                        record_start integer,
+                        record_end integer,
+                        offset integer
+                        )''')
+                if self._sql_index or unique:
+                    c.execute(f'''CREATE {"UNIQUE" if unique else ""} INDEX IF NOT EXISTS key_index ON MDX_INDEX (key_text)''')
+                    
+                tuple_list = [(item['key_text'], item['file_pos'],
+                            item['compressed_size'], item['decompressed_size'],
+                            item['record_block_type'], item['record_start'],
+                            item['record_end'], item['offset'])
+                            for item in index_list]
+                c.executemany('INSERT INTO MDX_INDEX VALUES (?,?,?,?,?,?,?,?)',
+                            tuple_list)
 
-        tuple_list = [(item['key_text'], item['file_pos'],
-                       item['compressed_size'], item['decompressed_size'],
-                       item['record_block_type'], item['record_start'],
-                       item['record_end'], item['offset'])
-                      for item in index_list]
-        c.executemany('INSERT INTO MDX_INDEX VALUES (?,?,?,?,?,?,?,?)',
-                      tuple_list)
-        if self._sql_index:
-            c.execute('''
-                CREATE UNIQUE INDEX key_index ON MDX_INDEX (key_text)
-                ''')
-
-        conn.commit()
-        conn.close()
-
+                    
+    def get_connection(self, _local, db_path) -> sqlite3.Connection:
+        """Retrieves or creates a thread-unique SQLite connection."""
+        if not hasattr(_local, "connection"):
+            # check_same_thread=True is default, reinforcing thread safety
+            _local.connection = sqlite3.connect(
+                db_path,
+                timeout=20.0  # Prevents immediate locking errors under concurrency
+            )
+            # Enable foreign keys per connection
+            _local.connection.execute("PRAGMA foreign_keys = ON;")
+        return _local.connection
+    
     @staticmethod
     def get_data_by_index(fmdx, index):
         fmdx.seek(index['file_pos'])
@@ -245,7 +207,7 @@ class IndexBuilder(object):
             # lzo compression
         elif record_block_type == 1:
             if lzo is None:
-                print("LZO compression is not supported")
+                raise Exception("LZO compression is not supported")
                 # decompress
             header = b'\xf0' + pack('>I', index['decompressed_size'])
             _record_block = lzo.decompress(
@@ -256,25 +218,28 @@ class IndexBuilder(object):
         elif record_block_type == 2:
             # decompress
             _record_block = zlib.decompress(record_block_compressed[8:])
+        else:
+            raise ValueError(f"Unsupported record block type: {record_block_type}")
+        
         data = _record_block[index['record_start'] -
                              index['offset']:index['record_end'] -
                              index['offset']]
         return data
 
-    def get_mdx_by_index(self, fmdx, index):
+    def get_md_by_index(self, fmdx, index, decode):
         data = self.get_data_by_index(fmdx, index)
-        record = data.decode(
-            self._encoding, errors='ignore').strip(u'\x00').encode('utf-8')
+        
+        if not decode:
+            return data
+        
+        record = data.decode(self._encoding, errors='ignore').strip(u'\x00').encode('utf-8')
         if self._stylesheet:
             record = self._replace_stylesheet(record)
         record = record.decode('utf-8')
         return record
 
-    def get_mdd_by_index(self, fmdx, index):
-        return self.get_data_by_index(fmdx, index)
-
     @staticmethod
-    def lookup_indexes(db, keyword, ignorecase=None):
+    def lookup_indexes(conn, keyword, ignorecase=None):
         indexes = []
         if ignorecase:
             sql = 'SELECT * FROM MDX_INDEX WHERE lower(key_text) = lower("{}")'.format(
@@ -282,7 +247,8 @@ class IndexBuilder(object):
         else:
             sql = 'SELECT * FROM MDX_INDEX WHERE key_text = "{}"'.format(
                 keyword)
-        with sqlite3.connect(db) as conn:
+        # with sqlite3.connect(db) as conn:  # leaks fd hanlder
+        with conn:
             cursor = conn.execute(sql)
             for result in cursor:
                 index = {}
@@ -296,27 +262,25 @@ class IndexBuilder(object):
                 indexes.append(index)
         return indexes
 
-    def mdx_lookup(self, keyword, ignorecase=None):
+    def md_lookup(self, index_db_conn, mdict_file, keyword, decode, ignorecase=None):
         lookup_result_list = []
-        indexes = self.lookup_indexes(self._mdx_db, keyword, ignorecase)
-        with open(self._mdx_file, 'rb') as mdx_file:
+        indexes = self.lookup_indexes(index_db_conn, keyword, ignorecase)
+        with open(mdict_file, 'rb') as md_file:
             for index in indexes:
-                lookup_result_list.append(
-                    self.get_mdx_by_index(mdx_file, index))
+                lookup_result_list.append(self.get_md_by_index(md_file, index, decode))
         return lookup_result_list
+    
+    def mdx_lookup(self, keyword, ignorecase=None):
+        conn = self.get_connection(self._tlocal_mdx_db_conn, self._mdx_db_path)
+        return self.md_lookup(conn, self._mdx_file, keyword, True)
 
     def mdd_lookup(self, keyword, ignorecase=None):
-        lookup_result_list = []
-        indexes = self.lookup_indexes(self._mdd_db, keyword, ignorecase)
-        with open(self._mdd_file, 'rb') as mdd_file:
-            for index in indexes:
-                lookup_result_list.append(
-                    self.get_mdd_by_index(mdd_file, index))
-        return lookup_result_list
+        conn = self.get_connection(self._tlocal_mdd_db_conn, self._mdd_db_path)
+        return self.md_lookup(conn, self._mdd_file, keyword, False)
 
     @staticmethod
-    def get_keys(db, query=''):
-        if not db:
+    def get_keys(conn, query=''):
+        if not conn:
             return []
         if query:
             if '*' in query:
@@ -326,16 +290,18 @@ class IndexBuilder(object):
             sql = 'SELECT key_text FROM MDX_INDEX WHERE key_text LIKE \"' + query + '\"'
         else:
             sql = 'SELECT key_text FROM MDX_INDEX'
-        with sqlite3.connect(db) as conn:
+        with conn:
             cursor = conn.execute(sql)
             keys = [item[0] for item in cursor]
             return keys
 
-    def get_mdd_keys(self, query=''):
-        return self.get_keys(self._mdd_db, query)
-
     def get_mdx_keys(self, query=''):
-        return self.get_keys(self._mdx_db, query)
+        conn = self.get_connection(self._tlocal_mdx_db_conn, self._mdx_db_path)
+        return self.get_keys(conn, query)
+
+    def get_mdd_keys(self, query=''):
+        conn = self.get_connection(self._tlocal_mdd_db_conn, self._mdd_db_path)
+        return self.get_keys(conn, query)
 
 
 # mdx_builder = IndexBuilder("oald.mdx")
@@ -344,5 +310,5 @@ class IndexBuilder(object):
 # keys1 = mdx_builder.get_mdx_keys('abstrac')
 # keys2 = mdx_builder.get_mdx_keys('*tion')
 # for key in keys2:
-# text = mdx_builder.mdx_lookup(key)[0]
+#   text = mdx_builder.mdx_lookup(key)[0]
 # pass
